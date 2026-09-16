96bca19bb0ceac50358c9f2962788f9dbf982a1b
Qiaoyi <haflashahia@gmail.com>
2026-09-16 23:56:15 +0700

    feat: menambahkan CRUD dashboard

--- js/script.js
+++ js/script.js
@@ -7,8 +7,18 @@ const jurusan = document.getElementById("jurusan");
 const kegiatan = document.getElementById("kegiatan");
 const syarat = document.getElementById("syarat");
 const tombol = document.getElementById("btndaftar");
-
 const hasil = document.getElementById("hasil");
+const status = document.getElementById("status");
+
+let daftarpendaftar = JSON.parse(localStorage.getItem("daftarpendaftar")) || [];
+let editId = null;
+
+const params = new URLSearchParams(window.location.search);
+const idEdit = params.get("edit");
+
+status.addEventListener("change", function (){
+    status.classList.add("selected");
+});
 
 // mengubah tampilan select kelas setelah user memilih kelas//
 kelas.addEventListener("change", function (){
@@ -25,6 +35,36 @@ syarat.addEventListener("change", function () {
     tombol.disabled = !syarat.checked;
 });
 
+function isiFormEdit(id) {
+        const peserta = daftarpendaftar.find (function (data) {
+            return data.id === id;
+        });
+
+         if (!peserta) {
+        return;
+    }
+
+    nama.value = peserta.nama;
+    email.value = peserta.email;
+    kelas.value = peserta.kelas;
+    jurusan.value = peserta.jurusan;
+    kegiatan.value = peserta.kegiatan;
+    status.value = peserta.status || "pending";
+
+    kelas.classList.add("selected");
+    jurusan.classList.add("selected");
+
+    editId = id;
+
+    status.disabled = false;
+    syarat.checked = true;
+
+    tombol.disabled = false;
+    tombol.textContent = "perbarui data";
+
+    hasil.innerHTML = "";
+}
+
 // menjalankan validasi ketika form dikirim//
 form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -34,16 +74,18 @@ form.addEventListener("submit", function (event) {
     document.getElementById("errorkelas").textContent = "";
     document.getElementById("errorjurusan").textContent = "";
     document.getElementById("errorkegiatan").textContent = "";
+    document.getElementById("errorstatus").textContent = "";
     
     nama.style.border = "";
     email.style.border = "";
     kelas.style.border = "";
     jurusan.style.border = "";
     kegiatan.style.border = "";
+    status.style.border = "";
 
     hasil.innerHTML = "";
 
-    let valid = true
+    let valid = true;
 
 //validasi nama//
     if (nama.value.trim() === "") {
@@ -92,6 +134,12 @@ if(kegiatan.value.trim() === "") {
         valid = false;
     }
 
+//validasi status//
+if(status.value.trim() === "") {
+        document.getElementById("errorstatus").textContent = "*Silahkan pilih status.";
+        status.style.border = "2px solid red";
+        valid = false;
+    }
 //validasi syarat dan ketentuan//
 if (!syarat.checked) {
     hasil.innerHTML = "Anda harus menyetujui syarat & ketentuan.";
@@ -99,9 +147,23 @@ if (!syarat.checked) {
     valid = false;
 }
 
-//menampilkan hasil jika semua data valid//
-if (valid) {
-    hasil.innerHTML = `<strong>Pendaftaran berhasil!</strong>
+if (!valid) {return;
+}
+
+if(editId === null) {
+    const peserta = {
+        id: Date.now(),
+        nama: nama.value.trim(),
+        email: email.value.trim(),
+        kelas: kelas.value,
+        jurusan: jurusan.value,
+        kegiatan: kegiatan.value.trim(),
+        status: "pending",
+          };
+
+daftarpendaftar.push(peserta);
+
+hasil.innerHTML = `<strong>Pendaftaran berhasil!</strong>
     <br></br>
     nama: ${nama.value}
     <br>
@@ -115,17 +177,54 @@ if (valid) {
 
     hasil.style.color = "White";
 
+    }else {
+        const index = daftarpendaftar.findIndex(function(data) {
+            return data.id === editId;
+        });
+
+    if (index !== -1) {
+
+        daftarpendaftar[index].nama = nama.value;
+        daftarpendaftar[index].email = email.value;
+        daftarpendaftar[index].kelas = kelas.value;
+        daftarpendaftar[index].jurusan = jurusan.value;
+        daftarpendaftar[index].kegiatan = kegiatan.value;
+        daftarpendaftar[index].status = status.value;
+
+        hasil.innerHTML = "<strong>Data berhasil Diperbarui!</strong>";
+        hasil.style.color = "White";
+    }
+}
+
+localStorage.setItem(
+        "daftarpendaftar",
+        JSON.stringify(daftarpendaftar)
+    );
+
     form.reset ();
     kelas.classList.remove ("selected");
     jurusan.classList.remove ("selected");
+    status.disabled = true;
     tombol.disabled = true;
+    tombol.textContent = "Daftar Sekarang";
 
-    console.log("Data_pendaftaran;", {
-        nama: nama.value,
-        email: email.value,
-        kelas: kelas.value,
-        jurusan: jurusan.value,
-        kegiatan: kegiatan.value
-    })
-}
+    editId = null;
+
+    if (idEdit) {
+        window.history.replaceState(
+            {},
+            document.title,
+            window.location.pathname
+        );
+    }
+
+    console.log(
+        "Data_pendaftaran;", 
+        daftarpendaftar
+    );
 });
+
+    if (idEdit) {
+        isiFormEdit(Number(idEdit));
+    }
+
